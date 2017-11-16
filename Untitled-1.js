23405af507e1c14b829cae910be15e023f30a4de
IRONMANMARK <33693731+IRONMANMARK@users.noreply.github.com>
2017-11-16 20:31:12 +0800

    Initial commit

---
+++ Untitled-1.js
@@ -0,0 +1,19 @@
+$(function() {
+        $('#navigation a').stop().animate({
+                'marginLeft': '-85px'
+        },
+        1000);
+
+        $('#navigation > li').hover(function() {
+                $('a', $(this)).stop().animate({
+                        'marginLeft': '-10px'
+                },
+                200);
+        },
+        function() {
+                $('a', $(this)).stop().animate({
+                        'marginLeft': '-85px'
+                },
+                200);
+        });
+});
